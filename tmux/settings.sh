c5770237c5736cc5cd0cbc9cf201c99c4f93d9a9
JinKanai <jin@kanaisan.com>
2020-05-28 12:54:37 +0900

    remove powerline-status

--- tmux/settings.sh
+++ tmux/settings.sh
@@ -1,18 +1,11 @@
 #!/bin/sh
 
-tmux_conf_path=`locate powerline.conf|grep python3`
-echo $tmux_conf_path
-tmux_path=`dirname $tmux_conf_path|sed -e "s/\/bindings\/tmux//g"`
-echo $tmux_path
-
-# copy configs to home.config
-cp -rf $tmux_path/config_files/* $HOME/.config/powerline/
-mv $HOME/.config/powerline/config.json $PWD/tmux/config.json.bak
-
+# copy base to conf
 cp -f $PWD/tmux/tmux.conf.base $PWD/tmux/tmux.conf
 
-# add powerline to tmux.conf
-echo "source $tmux_path/bindings/tmux/powerline.conf" >> $PWD/tmux/tmux.conf
+# add simple tmux settings
+echo "" >> $PWD/tmux/tmux.conf
+cat $PWD/tmux/tmux.conf.simple >> $PWD/tmux/tmux.conf
 
 # set fish as default shell
 fish_path=`which fish`
